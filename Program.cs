2566c15ee3d875d0fc09f22d70124c6731ec1857
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-09-14 18:01:18 -0300

    exercício feito

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+                double f, convc;
+                Console.Clear();
+                Console.Beep();
+                Console.Write("Digite a temperatura em °F:");
+                f= double.Parse(Console.ReadLine());
+                convc= (f-32/1.8);
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"{convc}°C");
+                Console.ResetColor();
+
+
+
+
+
+
+
+        }
+    }
+}
